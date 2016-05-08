becd0d841a084dcd3e8afaa40283b67c55b61625
luckyscript <jsjhlk@qq.com>
2016-05-08 13:33:28 +0800

    first version

---
+++ index.js
@@ -0,0 +1,79 @@
+var express = require('express');
+var http = require('http');
+var app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser')
+
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var detail = '';
+var configData;
+
+
+readFile("./config.json",function(data) {
+    configData = JSON.parse(data.toString());
+    var playList = configData.playList;
+    fetchDetail(playList,function (detail) {
+        var playListDetail = JSON.parse(detail);
+        var musicArr = playListDetail.result.tracks;
+        var musicNum = musicArr.length;
+        app.get("/app", function(req,res) {
+            console.log("get /");
+            var index = Math.floor(Math.random()*musicArr.length);
+            console.log(musicArr.length+" "+index);
+            musicData = {
+                artist: musicArr[index].artists[0].name,
+                pic: musicArr[index].album.picUrl,
+                name: musicArr[index].name,
+                mp3Url: musicArr[index].mp3Url
+            }
+            res.send(musicData);
+        });
+        
+        //musicArr[0].mp3Url;
+    })
+});
+
+/**
+* fetch playList detail
+*/
+function fetchDetail (id,callback) {
+	var r = http.request("http://music.163.com/api/playlist/detail?id="+id, function(res) {
+	    res.on("data", function(chunk) {
+	        detail += chunk.toString('utf8').trim();
+            setTimeout(function() {
+                callback(detail);
+            },3000)
+	    });
+	}).on("error", function (e) {
+	    console.log(e.message);
+	});
+	r.end();
+}
+
+/**
+* method that read File content
+*/
+function readFile(file, callback) {
+	fs.readFile(file, function (err,data) {
+		if (err) {
+			console.log("Unknown error,readfile failed" +err);
+		} else {
+			callback(data);
+		}
+	})
+}
+    
+
+//router
+/*app.get("/app", function(req,res) {
+    console.log("get /");
+})*/
+
+app.listen('3000', function() {
+    console.log("app running on server port 3000");
+})
+
---
+++ public/app.js
@@ -0,0 +1,42 @@
+(function($) {
+	var audioSrc;
+	function getMusic() {
+		$.ajax({
+			url: "/app",
+			method: "GET"
+		}).done(function(data) {
+			musicData = data;
+			document.querySelector('audio').src = musicData.mp3Url;
+			document.querySelector('h3').innerHTML = musicData.name;
+			document.querySelector('h4').innerHTML = musicData.artist;
+			document.querySelector('#music-pic').src = musicData.pic;
+			document.querySelector('audio').play();
+		})	
+	}
+	getMusic();
+	
+	setInterval(function() {
+		if (document.querySelector('audio').ended) {
+			getMusic();
+		}
+	},1000)
+
+	document.querySelector(".arrow")
+		.addEventListener("click", function() {
+			document.querySelector('audio').pause();
+			getMusic();
+		});
+
+	document.querySelector("#music-control")
+		.addEventListener("click", function() {
+			if (document.querySelector('audio').paused) {
+				document.querySelector('audio').play();
+				$('#music-pic').css("animation","6s imground linear infinite");
+				$(".fa").removeClass("fa-play").addClass("fa-pause");
+			} else {
+				document.querySelector('audio').pause();
+				$('#music-pic').css("animation","a");
+				$(".fa").removeClass("fa-pause").addClass("fa-play");
+			}
+		})
+})(jQuery);
